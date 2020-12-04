bb09e58c0a164531324f7786ebbf5e0c2952c5e2
aureliencarpentier <aurecarp@live.fr>
2020-12-04 12:09:27 +0100

    affichage des bieres

--- js/scriptQuery.js
+++ js/scriptQuery.js
@@ -2,13 +2,47 @@ function queryBeer() {
   let url = 'http://dbpedia.org/sparql';
   let query = [
     'PREFIX dbpedia2: <http://dbpedia.org/resource/> PREFIX dbpedia2: <http://dbpedia.org/property/>',
-    'select ?biere where{',
-    '{{?biere dbo:product dbr:Beer.}',
+    'PREFIX plg: <http://purl.org/linguistics/gold/>',
+    'SELECT DISTINCT ?beer WHERE {',
+
+    '{',
+    '{?beer dbp:type dbr:Beer. }',
     'UNION',
-    '{?biere dbp:type dbr:Beer. }',
+    '{?e skos:broader  dbc:Beer_by_country.',
+    '?beer dct:subject ?e .}',
     'UNION',
-    '{dbr:Beer dbo:product ?biere.}}',
+    '{?beer plg:hypernym dbr:Beer}',
     '}',
+
+    'Minus',
+    '{?beer rdfs:label ?label.',
+    'filter regex(?label, "Beer in"). }',
+    'Minus',
+    '{?beer rdfs:label ?label.',
+    'filter regex(?label, "Brewer"). }',
+    'Minus',
+    '{?beer rdfs:label ?label.',
+    'filter regex(?label, "List"). }',
+    'Minus',
+    '{?beer dbo:type dbr:Brewery.}',
+    'Minus',
+    '{?beer dbo:type dbr:Brewing.}',
+    'Minus',
+    '{?beer dbo:type dbo:Company.}',
+    'Minus',
+    '{?beer rdf:type dbo:Company.}',
+    'Minus',
+    '{?beer rdf:type dbo:Brewery.}',
+    'Minus',
+    '{?beer plg:hypernym dbr:Museum}',
+    'Minus',
+    '{?beer plg:hypernym dbr:Brewery}',
+    'Minus',
+    '{?beer plg:hypernym dbr:Company}',
+    'Minus',
+    '{?beer plg:hypernym dbr:Group}',
+    '}',
+    'ORDER BY ASC(?beer)',
   ].join(' ');
 
   var queryURL = encodeURI(url + '?query=' + query + '&format=json');
@@ -22,12 +56,13 @@ function queryBeer() {
       console.log(data);
       let beers = data.results.bindings;
       for (let x in beers) {
-        let link = beers[x].biere.value;
-          let name = link.substring(link.lastIndexOf("/")+1);
-          let line = document.createElement('a');
-        line.setAttribute("href", "#");
-        line.style.display = "block";
-        line.addEventListener("click", () => console.log("test"));
+        let link = beers[x].beer.value;
+        let name = link.substring(link.lastIndexOf('/') + 1);
+        let line = document.createElement('a');
+        line.setAttribute('href', '#');
+        line.setAttribute('link', link);
+        line.style.display = 'block';
+        line.addEventListener('click', () => console.log('test'));
         line.append(name);
         searchResult.appendChild(line);
       }
